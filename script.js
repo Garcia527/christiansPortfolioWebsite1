42b3dba4a038428a43f4153c9c69c578d1b03fdd
Oscar Garcia <oscarg327@gmail.com>
2022-08-31 19:09:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+        //18:30-26:33
+    });
+});
